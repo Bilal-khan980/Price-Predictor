0c6994923ff23b1dc9a3ee438b5da09d2146177e
Bilal-khan980 <bilal4412192@gmail.com>
2024-09-15 06:18:33 -0700

    testing

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import requests
+import subprocess
+import time
 
 
 # Assuming Flask server is running locally on port 5000
 url = 'http://localhost:5000/predict'
 
+def start_server():
+    # Start the Flask server in the background
+    subprocess.Popen(['flask', 'run', '--host=0.0.0.0', '--port=5000'])
+    # Give the server a few seconds to start
+    time.sleep(5)
 
 def test_predict():
+    start_server()
+    
     # Input data for testing
     data = {
         'sqft': 1500,
@@ -21,7 +30,6 @@ def test_predict():
 
     # Assert that the response contains the predicted price
     assert "Predicted House Price" in response.text
-
 
 if __name__ == "__main__":
     test_predict()
