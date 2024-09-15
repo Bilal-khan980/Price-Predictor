5915bf27e67c7976a052c3f397202343bdce142f
Bilal-khan980 <bilal4412192@gmail.com>
2024-09-15 06:12:39 -0700

    testing

--- test.py
+++ test.py
@@ -1,27 +1,16 @@
-import pytest
-from app import app  # Import your Flask app here
-
-# Create a test client using the Flask app
-client = app.test_client()
-
+from app import app
 
 def test_predict():
-    # Input data for testing
+    client = app.test_client()
     data = {
         'sqft': 1500,
         'bedrooms': 3,
         'bathrooms': 2
     }
-
-    # Sending POST request
     response = client.post('/predict', data=data)
-
-    # Assert that the response status code is 200
     assert response.status_code == 200
-
-    # Assert that the response contains the predicted price
     assert "Predicted House Price" in response.data.decode()
 
-
 if __name__ == "__main__":
+    import pytest
     pytest.main()
