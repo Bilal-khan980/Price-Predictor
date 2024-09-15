edf843de6bf9795fcdff0387c79980583a229a8a
Bilal-khan980 <bilal4412192@gmail.com>
2024-09-15 06:01:28 -0700

    testing

--- test.py
+++ test.py
@@ -15,10 +15,10 @@ def test_predict():
 
     # Sending POST request
     response = client.post('/predict', data=data)
-    
+
     # Assert that the response status code is 200
     assert response.status_code == 200
-    
+
     # Assert that the response contains the predicted price
     assert "Predicted House Price" in response.data.decode()
 
